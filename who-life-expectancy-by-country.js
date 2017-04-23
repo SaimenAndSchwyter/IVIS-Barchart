c961568fb613c4bca88f88fa4e5de75d2beb6d76
Simon Schwyter <simon.schwyter@students.fhnw.ch>
2017-04-23 15:56:44 +0200

    Barchart

---
+++ who-life-expectancy-by-country.js
@@ -0,0 +1,12 @@
+var _data ={"data":[{"Country":"Switzerland","Life_expectancy_at_birth":"82.6"},
+{"Country":"Japan","Life_expectancy_at_birth":"82.5"},
+{"Country":"Singapore","Life_expectancy_at_birth":"82.2"},
+{"Country":"Spain","Life_expectancy_at_birth":"82.1"},
+{"Country":"Iceland","Life_expectancy_at_birth":"82.1"},
+{"Country":"Australia","Life_expectancy_at_birth":"82"},
+{"Country":"Italy","Life_expectancy_at_birth":"82"},
+{"Country":"Israel","Life_expectancy_at_birth":"81.8"},
+{"Country":"Sweden","Life_expectancy_at_birth":"81.7"},
+{"Country":"France","Life_expectancy_at_birth":"81.7"}
+
+]}
